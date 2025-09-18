9976f88812bd9980b6ba55f0b9400707079f340f
gaaiuss <caio.gui.castro@gmail.com>
2025-09-18 10:43:46 -0300

    finished

--- task_list/assets/js/main.js
+++ task_list/assets/js/main.js
@@ -7,10 +7,49 @@ function createLi() {
     return li
 }
 
+function createClearButton(btnText, li) {
+    li.innerText += ' ';
+    const btn = document.createElement('button');
+    btn.innerText = btnText;
+    btn.setAttribute('class', 'clear');
+    btn.setAttribute('title', 'Clear this task');
+    li.appendChild(btn);
+}
+
+function saveTask() {
+    const taskLmt = tasks.querySelectorAll('li');
+    const taskList = []
+
+    for (let task of taskLmt) {
+        let taskText = task.innerText;
+        taskText = taskText.replace('Clear', '').trim();
+        taskList.push(taskText);
+    }
+    const taskJson = JSON.stringify(taskList);
+    localStorage.setItem('tasks', taskJson)
+}
+
 function createTask(text) {
     const li = createLi();
     li.innerText = text;
-    tasks.appendChild(li)
+    tasks.appendChild(li);
+    clearInput();
+    createClearButton('Clear', li);
+    saveTask();
+}
+
+function clearInput(){
+    inputTask.value = '';
+    inputTask.focus();
+}
+
+function addSavedTasks() {
+    const tasks = localStorage.getItem('tasks');
+    const taskList = JSON.parse(tasks)
+    
+    for (let task of taskList) {
+        createTask(task);
+    }
 }
 
 btnTask.addEventListener('click', function(){
@@ -24,3 +63,14 @@ inputTask.addEventListener('keypress', function(e){
         createTask(inputTask.value);
     }
 })
+
+document.addEventListener('click', function(e){
+    const lmt = e.target;
+
+    if (lmt.classList.contains('clear')){
+        lmt.parentElement.remove();
+        saveTask();
+    }
+});
+
+addSavedTasks();
